217dc462657beef43ebe20855898253aca0125e0
Benjamin M. Marta <108314514+BenjaminMMarta@users.noreply.github.com>
2023-04-23 14:17:50 -0700

    Added app.js file containing 'How The Web Works' Subunit 19.1 Subunit Exercise.

---
+++ app.js
@@ -0,0 +1,38 @@
+//--------------------How Web Works--------------------//
+
+/*
+1). What is HTTP? 
+HTTP stands for Hypertext Transfer Protocol. It's a set of rules that states how to communicate between a browser and a web server. 
+
+2). What is a URL?
+URL stands for "Universial Resource Locators". It's an address of a given unique resource on the Web. It's also known as a web adress. 
+
+3). What is DNS?
+DNS stands for "Domain Name System". It acts like a phone book for the web. It's purpose is to take host name like "Facebook.com" and translate it into an IP Adress.
+
+4). What is a query string?
+A query string is a portion of the URL which assigns values in key/value pairs to parameters.
+
+5). What are two HTTP verbs and how are they different?
+"GET" gets information from a server. This doesn't change server data.
+"POST" sends information to a server. This does change server data.
+
+6). What is an HTTP request?
+A HTTP request is a request the client sends to a server.
+
+7). What is an HTTP response?
+A HTTP response is a response the server returns to your client.  
+
+8). What is an HTTP header? Give a couple examples of request and response headers you have seen.
+A header is request information sent from the web browser to the server. 
+Example request headers: Cookie and Host.
+Example response headers: Content-Type and Last-Modified.
+
+9). What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+The browser turns the web adress into an IP address.
+The browser makes a request to the IP address.
+The server sends a response.
+The browser makes content appear from the HTML, and other information like CSS and JavaScript if available.
+*/
+
+//--------------------End--------------------//
